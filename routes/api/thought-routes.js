d3373853754a5a77858831e32d352ae0923ed4c1
Hunter <hunterde91@gmail.com>
2024-05-28 20:31:50 -0400

    added routing

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,11 +2,19 @@ const router = require("express").Router();
 const {} = require("../../controllers/thought-controller");
 
 // /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,9 +2,16 @@ const router = require("express").Router();
 const {} = require("../../controllers/user-controller");
 
 // /api/users
+router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:userId
+router
+  .route("/:userId")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
